76c861e5575e64660c6af772c43ae3ba793e2049
OneKekBer <iliaberezovsky227@gmail.com>
2024-08-09 10:53:00 +0200

    good

--- client/src/hooks/useFetch.ts
+++ client/src/hooks/useFetch.ts
@@ -1,30 +1,30 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 
-const useFetch = <T>(url: string) => {
-	const [data, setData] = useState<T | null>(null)
-	const [loading, setLoading] = useState<boolean>(true)
-	const [error, setError] = useState<string | null>(null)
+// const useFetch = <T>(url: string) => {
+// 	const [data, setData] = useState<T | null>(null)
+// 	const [loading, setLoading] = useState<boolean>(true)
+// 	const [error, setError] = useState<string | null>(null)
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const response = await fetch(url)
-				if (!response.ok) {
-					throw new Error('Network response was not ok')
-				}
-				const result = await response.json()
-				setData(result)
-			} catch (err) {
-				setError(err.message)
-			} finally {
-				setLoading(false)
-			}
-		}
+// 	useEffect(() => {
+// 		const fetchData = async () => {
+// 			try {
+// 				const response = await fetch(url)
+// 				if (!response.ok) {
+// 					throw new Error('Network response was not ok')
+// 				}
+// 				const result = await response.json()
+// 				setData(result)
+// 			} catch (err) {
+// 				setError(err.message)
+// 			} finally {
+// 				setLoading(false)
+// 			}
+// 		}
 
-		fetchData()
-	}, [url])
+// 		fetchData()
+// 	}, [url])
 
-	return { data, loading, error }
-}
+// 	return { data, loading, error }
+// }
 
-export default useFetch
+// export default useFetch
