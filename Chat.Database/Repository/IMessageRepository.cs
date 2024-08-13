743556189368521cbcab3b4068802a1e08746c1f
OneKekBer <iliaberezovsky227@gmail.com>
2024-08-13 19:25:13 +0200

    s

--- Chat.Database/Repository/IMessageRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using API.Models;
-
-namespace API.Repository
-{
-    public interface IMessageRepository : IRepository<Message>
-    {
-        public Task<IEnumerable<Message>> GetChatMessages(Guid chatId);
-    }
-}
